ec0f5700e036876eda17d7ab508e9f3f47b9189b
Elean <eric.lean@icloud.com>
2026-05-21 01:42:09 +0200

    blink-cmd connected to lsp

--- init.lua
+++ init.lua
@@ -415,6 +415,91 @@ do
     -- Search Neovim config files
     vim.keymap.set('n', '<leader>sn', function() fzf.files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
 end
+
+-- ============================================================
+-- SECTION 7: AUTOCOMPLETE & SNIPPETS
+-- blink.cmp and luasnip setup
+-- ============================================================
+do
+    -- [[ Snippet Engine ]]
+
+    -- NOTE: You can also specify plugin using a version range for its git tag.
+    --  See `:help vim.version.range()` for more info
+    vim.pack.add { { src = gh 'L3MON4D3/LuaSnip', version = vim.version.range '2.*' } }
+    require('luasnip').setup {}
+
+    -- `friendly-snippets` contains a variety of premade snippets.
+    --    See the README about individual language/framework/plugin snippets:
+    --    https://github.com/rafamadriz/friendly-snippets
+    --
+    -- vim.pack.add { gh 'rafamadriz/friendly-snippets' }
+    -- require('luasnip.loaders.from_vscode').lazy_load()
+
+    -- [[ Autocomplete Engine ]]
+    vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
+    require('blink.cmp').setup {
+        keymap = {
+            -- 'default' (recommended) for mappings similar to built-in completions
+            --   <c-y> to accept ([y]es) the completion.
+            --    This will auto-import if your LSP supports it.
+            --    This will expand snippets if the LSP sent a snippet.
+            -- 'super-tab' for tab to accept
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- For an understanding of why the 'default' preset is recommended,
+            -- you will need to read `:help ins-completion`
+            --
+            -- No, but seriously. Please read `:help ins-completion`, it is really good!
+            --
+            -- All presets have the following mappings:
+            -- <tab>/<s-tab>: move to right/left of your snippet expansion
+            -- <c-space>: Open menu or open docs if already open
+            -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+            -- <c-e>: Hide menu
+            -- <c-k>: Toggle signature help
+            --
+            -- See `:help blink-cmp-config-keymap` for defining your own keymap
+            preset = 'enter',
+            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
+
+            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        },
+
+        appearance = {
+            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = 'normal',
+        },
+
+        completion = {
+            -- By default, you may press `<c-space>` to show the documentation.
+            -- Optionally, set `auto_show = true` to show the documentation after a delay.
+            documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        },
+
+        sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+
+        snippets = { preset = 'luasnip' },
+
+        -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+        -- which automatically downloads a prebuilt binary when enabled.
+        --
+        -- By default, we use the Lua implementation instead, but you may enable
+        -- the rust implementation via `'prefer_rust_with_warning'`
+        --
+        -- See `:help blink-cmp-config-fuzzy` for more information
+        fuzzy = { implementation = 'prefer_rust_with_warning' },
+
+        -- Shows a signature help window while you type arguments for a function
+        signature = { enabled = true },
+    }
+end
+
 -- ============================================================
 -- SECTION 5: LSP
 -- LSP keymaps, server configuration, Mason tools installations
@@ -598,6 +683,7 @@ do
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     for name, server in pairs(servers) do
+        server.capabilities = require('blink.cmp').get_lsp_capabilities(server.capabilities)
         vim.lsp.config(name, server)
         vim.lsp.enable(name)
     end
@@ -640,91 +726,6 @@ do
 
     vim.keymap.set({ 'n', 'v' }, '<leader>f', function() require('conform').format { async = true } end, { desc = '[F]ormat buffer' })
 end
-
--- ============================================================
--- SECTION 7: AUTOCOMPLETE & SNIPPETS
--- blink.cmp and luasnip setup
--- ============================================================
-do
-    -- [[ Snippet Engine ]]
-
-    -- NOTE: You can also specify plugin using a version range for its git tag.
-    --  See `:help vim.version.range()` for more info
-    vim.pack.add { { src = gh 'L3MON4D3/LuaSnip', version = vim.version.range '2.*' } }
-    require('luasnip').setup {}
-
-    -- `friendly-snippets` contains a variety of premade snippets.
-    --    See the README about individual language/framework/plugin snippets:
-    --    https://github.com/rafamadriz/friendly-snippets
-    --
-    -- vim.pack.add { gh 'rafamadriz/friendly-snippets' }
-    -- require('luasnip.loaders.from_vscode').lazy_load()
-
-    -- [[ Autocomplete Engine ]]
-    vim.pack.add { { src = gh 'saghen/blink.cmp', version = vim.version.range '1.*' } }
-    require('blink.cmp').setup {
-        keymap = {
-            -- 'default' (recommended) for mappings similar to built-in completions
-            --   <c-y> to accept ([y]es) the completion.
-            --    This will auto-import if your LSP supports it.
-            --    This will expand snippets if the LSP sent a snippet.
-            -- 'super-tab' for tab to accept
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- For an understanding of why the 'default' preset is recommended,
-            -- you will need to read `:help ins-completion`
-            --
-            -- No, but seriously. Please read `:help ins-completion`, it is really good!
-            --
-            -- All presets have the following mappings:
-            -- <tab>/<s-tab>: move to right/left of your snippet expansion
-            -- <c-space>: Open menu or open docs if already open
-            -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-            -- <c-e>: Hide menu
-            -- <c-k>: Toggle signature help
-            --
-            -- See `:help blink-cmp-config-keymap` for defining your own keymap
-            preset = 'enter',
-            ['<Tab>'] = { 'select_next', 'fallback' },
-            ['<S-Tab>'] = { 'select_prev', 'fallback' },
-
-            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        },
-
-        appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'normal',
-        },
-
-        completion = {
-            -- By default, you may press `<c-space>` to show the documentation.
-            -- Optionally, set `auto_show = true` to show the documentation after a delay.
-            documentation = { auto_show = false, auto_show_delay_ms = 500 },
-        },
-
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-
-        snippets = { preset = 'luasnip' },
-
-        -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-        -- which automatically downloads a prebuilt binary when enabled.
-        --
-        -- By default, we use the Lua implementation instead, but you may enable
-        -- the rust implementation via `'prefer_rust_with_warning'`
-        --
-        -- See `:help blink-cmp-config-fuzzy` for more information
-        fuzzy = { implementation = 'prefer_rust_with_warning' },
-
-        -- Shows a signature help window while you type arguments for a function
-        signature = { enabled = true },
-    }
-end
-
 -- ============================================================
 -- SECTION 8: TREESITTER
 -- Parser installation, syntax highlighting, folds, indentation
