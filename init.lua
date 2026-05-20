660fffb057c0847cf05191ad971854c91d5eae3d
Elean1402 <84944566+Elean1402@users.noreply.github.com>
2026-05-20 18:05:18 +0200

    changed to fzf

--- init.lua
+++ init.lua
@@ -356,142 +356,65 @@ do
     -- ... and there is more!
     --  Check out: https://github.com/nvim-mini/mini.nvim
 end
-
 -- ============================================================
 -- SECTION 4: SEARCH & NAVIGATION
--- Telescope setup, keymaps, LSP picker mappings
+-- fzf-lua setup, keymaps, LSP picker mappings
 -- ============================================================
 do
-    -- [[ Fuzzy Finder (files, lsp, etc) ]]
-    --
-    -- Telescope is a fuzzy finder that comes with a lot of different things that
-    -- it can fuzzy find! It's more than just a "file finder", it can search
-    -- many different aspects of Neovim, your workspace, LSP, and more!
-    --
-    -- There are lots of other alternative pickers (like snacks.picker, or fzf-lua)
-    -- so feel free to experiment and see what you like!
-    --
-    -- The easiest way to use Telescope, is to start by doing something like:
-    --  :Telescope help_tags
-    --
-    -- After running this command, a window will open up and you're able to
-    -- type in the prompt window. You'll see a list of `help_tags` options and
-    -- a corresponding preview of the help.
-    --
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-    --
-    -- This opens a window that shows you all of the keymaps for the current
-    -- Telescope picker. This is really useful to discover what Telescope can
-    -- do as well as how to actually do it!
+    vim.pack.add { gh 'ibhagwan/fzf-lua' }
 
-    ---@type (string|vim.pack.Spec)[]
-    local telescope_plugins = {
-        gh 'nvim-lua/plenary.nvim',
-        gh 'nvim-telescope/telescope.nvim',
-        gh 'nvim-telescope/telescope-ui-select.nvim',
-    }
-    if vim.fn.executable 'make' == 1 then table.insert(telescope_plugins, gh 'nvim-telescope/telescope-fzf-native.nvim') end
+    local fzf = require 'fzf-lua'
 
-    -- NOTE: You can install multiple plugins at once
-    vim.pack.add(telescope_plugins)
-
-    -- See `:help telescope` and `:help telescope.setup()`
-    require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
-        extensions = {
-            ['ui-select'] = { require('telescope.themes').get_dropdown() },
-        },
+    fzf.setup {
+        'default',
+        ui_select = { winopts = { height = 0.33, width = 0.33 } },
     }
 
-    -- Enable Telescope extensions if they are installed
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+    vim.keymap.set('n', '<leader>sh', fzf.helptags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', fzf.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', fzf.files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>ss', fzf.builtin, { desc = '[S]earch [S]elect fzf-lua' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>sw', fzf.grep_cword, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', fzf.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', fzf.diagnostics_workspace, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sr', fzf.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>s.', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>sc', fzf.commands, { desc = '[S]earch [C]ommands' })
+    vim.keymap.set('n', '<leader><leader>', fzf.buffers, { desc = '[ ] Find existing buffers' })
 
-    -- See `:help telescope.builtin`
-    local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-    -- Add Telescope-based LSP pickers when an LSP attaches to a buffer.
-    -- If you later switch picker plugins, this is where to update these mappings.
+    -- LSP pickers on LspAttach
     vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('telescope-lsp-attach', { clear = true }),
+        group = vim.api.nvim_create_augroup('fzf-lsp-attach', { clear = true }),
         callback = function(event)
             local buf = event.buf
-
-            -- Find references for the word under your cursor.
-            vim.keymap.set('n', 'grr', builtin.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
-
-            -- Jump to the implementation of the word under your cursor.
-            -- Useful when your language has ways of declaring types without an actual implementation.
-            vim.keymap.set('n', 'gri', builtin.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
-
-            -- Jump to the definition of the word under your cursor.
-            -- This is where a variable was first declared, or where a function is defined, etc.
-            -- To jump back, press <C-t>.
-            vim.keymap.set('n', 'grd', builtin.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
-
-            -- Fuzzy find all the symbols in your current document.
-            -- Symbols are things like variables, functions, types, etc.
-            vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
-
-            -- Fuzzy find all the symbols in your current workspace.
-            -- Similar to document symbols, except searches over your entire project.
-            vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols, { buffer = buf, desc = 'Open Workspace Symbols' })
-
-            -- Jump to the type of the word under your cursor.
-            -- Useful when you're not sure what type a variable is and you want to see
-            -- the definition of its *type*, not where it was *defined*.
-            vim.keymap.set('n', 'grt', builtin.lsp_type_definitions, { buffer = buf, desc = '[G]oto [T]ype Definition' })
+            vim.keymap.set('n', 'grr', fzf.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
+            vim.keymap.set('n', 'gri', fzf.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
+            vim.keymap.set('n', 'gd', fzf.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
+            vim.keymap.set('n', 'gO', fzf.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
+            vim.keymap.set('n', 'gW', fzf.lsp_live_workspace_symbols, { buffer = buf, desc = 'Open Workspace Symbols' })
+            vim.keymap.set('n', 'grt', fzf.lsp_typedefs, { buffer = buf, desc = '[G]oto [T]ype Definition' })
         end,
     })
 
-    -- Override default behavior and theme when searching
-    vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-        })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    -- Fuzzy search in current buffer
+    vim.keymap.set(
+        'n',
+        '<leader>/',
+        function() fzf.grep_curbuf { winopts = { height = 0.33, width = 0.75 }, preview = { hidden = 'hidden' } } end,
+        { desc = '[/] Fuzzily search in current buffer' }
+    )
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+    -- Live grep across open buffers
     vim.keymap.set(
         'n',
         '<leader>s/',
-        function()
-            builtin.live_grep {
-                grep_open_files = true,
-                prompt_title = 'Live Grep in Open Files',
-            }
-        end,
+        function() fzf.live_grep { grep_open_files = true, prompt = 'Live Grep in Open Files> ' } end,
         { desc = '[S]earch [/] in Open Files' }
     )
 
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
+    -- Search Neovim config files
+    vim.keymap.set('n', '<leader>sn', function() fzf.files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
 end
-
 -- ============================================================
 -- SECTION 5: LSP
 -- LSP keymaps, server configuration, Mason tools installations
@@ -554,7 +477,7 @@ do
 
             -- WARN: This is not Goto Definition, this is Goto Declaration.
             --  For example, in C this would take you to the header.
-            map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
             -- The following two autocommands are used to highlight references of the
             -- word under your cursor when your cursor rests there for a little while.
@@ -604,6 +527,7 @@ do
         gopls = {},
         pyright = {},
         rust_analyzer = {},
+        zls = {},
         texlab = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -761,7 +685,9 @@ do
             -- <c-k>: Toggle signature help
             --
             -- See `:help blink-cmp-config-keymap` for defining your own keymap
-            preset = 'default',
+            preset = 'enter',
+            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
 
             -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
             --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -869,7 +795,7 @@ do
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
     -- place them in the correct locations.
-
+    vim.g.python3_host_prog = vim.fn.expand '~/.venv/neovim/bin/python'
     -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
     --
     --  Here are some example plugins that I've included in the Kickstart repository.
--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -7,16 +7,36 @@ vim.pack.add {
 }
 
 vim.g.maplocalleader = ','
-vim.g.vimtex_syntax_enabled = 0
+
+-- Syntax & concealment
+vim.g.vimtex_syntax_conceal_disable = 1
+vim.g.vimtex_syntax_enabled = 1
+vim.g.vimtex_syntax_conceal = {
+    accents = 1,
+    ligatures = 1,
+    cites = 1,
+    fancy = 1,
+    math_bounds = 1,
+    math_delimiters = 1,
+    math_fracs = 1,
+    math_super_sub = 1,
+    math_symbols = 1,
+    sections = 0,
+    styles = 1,
+}
+
+-- PDF viewer
 if vim.fn.has 'mac' == 1 then
     vim.g.vimtex_view_method = 'skim'
 else
     vim.g.vimtex_view_method = 'zathura'
 end
-vim.g.vimtex_compiler_method = 'latexmk'
 
+-- Compiler
+vim.g.vimtex_compiler_method = 'latexmk'
 vim.g.vimtex_compiler_latexmk = {
     continuous = 1,
+    callback = 1,
     options = {
         '-verbose',
         '-file-line-error',
@@ -26,9 +46,51 @@ vim.g.vimtex_compiler_latexmk = {
     },
 }
 
+-- Insert mode mappings (enables \item continuation + math shortcuts)
+vim.g.vimtex_imaps_enabled = 1
+
+-- Completion
+vim.g.vimtex_complete_enabled = 1
+vim.g.vimtex_complete_close_braces = 1
+
+-- Folding
+vim.g.vimtex_fold_enabled = 0
+
+-- TOC
+vim.g.vimtex_toc_config = {
+    split_width = 30,
+    show_help = 0,
+}
+
+-- Quickfix: open on errors but don't steal focus, suppress noisy warnings
+vim.g.vimtex_quickfix_mode = 2
+vim.g.vimtex_quickfix_ignore_filters = {
+    'Underfull',
+    'Overfull',
+    'LaTeX Warning',
+}
+
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'tex',
-    callback = function() vim.cmd 'packadd vimtex' end,
+    callback = function()
+        vim.cmd 'packadd vimtex'
+
+        vim.keymap.set('n', '<localleader>lt', function() return require('vimtex.fzf-lua').run() end, { buffer = true, desc = 'VimTeX ToC (fzf)' })
+
+        -- Continue \item on Enter in insert mode
+        vim.keymap.set('i', '<CR>', function()
+            local line = vim.api.nvim_get_current_line()
+
+            if line:match '^%s*\\item' then return '\n\\item ' end
+
+            return '\n'
+        end, {
+            buffer = true,
+            expr = true,
+            replace_keycodes = false,
+            desc = 'Continue LaTeX itemize',
+        })
+    end,
 })
 
 vim.lsp.config('texlab', {
